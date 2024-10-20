0bcbe4b73357714f4a1c802705050cf37f091e36
Mario Winter <mariowinter.sg@gmail.com>
2024-10-20 23:15:29 +0200

    fix(market_app): correct MarketSerializer class inheritance and id field type

--- market_app/api/serializers.py
+++ market_app/api/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
 from market_app.models import Market
 
-class MarketSerializer(serializers.Model):
-    id = serializers.ImageField(read_only=True)
+class MarketSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=255)
     location = serializers.CharField(max_length=255)
     description = serializers.CharField()
