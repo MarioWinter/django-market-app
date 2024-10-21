7e09ce8e44b391fd5c53a7dec881fdddc08aa2da
Mario Winter <mariowinter.sg@gmail.com>
2024-10-21 22:20:49 +0200

    feat(market_app): add API view for retrieving, creating, and deleting single market data using serializer

--- market_app/api/urls.py
+++ market_app/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import markets_view
+from .views import markets_view, single_market_view
 
 urlpatterns = [
     path('', markets_view),
+    path('<int:pk>', single_market_view),
 ]
+    
--- market_app/api/views.py
+++ market_app/api/views.py
@@ -13,11 +13,28 @@ def markets_view(request):
         serializer = MarketSerializer(markets, many=True)  # Der Serializer wird auf die Liste der Market-Objekte angewendet
         return Response(serializer.data)  # Die serialisierten Daten werden als Response zurückgegeben
     
+          
+    if request.method == "POST":  # Prüft, ob die HTTP-Methode POST ist
+        serializer = MarketSerializer(data=request.data)  # Erstellt einen Serializer mit den Daten aus der Anfrage
+        if serializer.is_valid():  # Überprüft, ob die Daten gültig sind (entsprechend den Regeln im Serializer)
+            serializer.save()  # Speichert die Daten in der Datenbank (erstellt ein neues Market-Objekt)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)  # Gibt die gespeicherten Daten als Antwort zurück
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  # Gibt Fehlermeldungen zurück, wenn die Daten ungültig sind
+
+
+
+@api_view(['GET', 'DELETE'])  # Dekorator, der angibt, dass diese View GET und DELETE Anfragen akzeptiert
+def single_market_view(request, pk):  # pk = primary Key, ist die ID aus der Datenbank
+    try:
+        market = Market.objects.get(pk=pk)  # Versucht, das Market-Objekt mit der gegebenen ID zu finden
+    except Market.DoesNotExist:  # Fängt den Fall ab, wenn kein Objekt mit dieser ID existiert
+        return Response(status=status.HTTP_404_NOT_FOUND)  # Gibt 404 Not Found zurück, wenn das Objekt nicht existiert
+        
+    if request.method == "GET":  # Wenn die Anfrage eine GET-Anfrage ist
+        serializer = MarketSerializer(market)  # Serialisiert das gefundene Market-Objekt
+        return Response(serializer.data)  # Gibt die serialisierten Daten als Antwort zurück
     
-    if request.method == "POST":
-        serializer = MarketSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.erros)
+    elif request.method == "DELETE":  # Wenn die Anfrage eine DELETE-Anfrage ist
+        market.delete()  # Löscht das Market-Objekt aus der Datenbank
+        return Response(status=status.HTTP_204_NO_CONTENT)  # Gibt 204 No Content zurück, um erfolgreiche Löschung anzuzeigen
+    
