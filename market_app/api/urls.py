6a813353846c932332b6bf4b8dcd3939d543389f
Mario Winter <mariowinter.sg@gmail.com>
2024-10-28 21:56:32 +0100

    feat(market_app): refactor views to use class-based views and viewsets

--- market_app/api/urls.py
+++ market_app/api/urls.py
@@ -1,12 +1,17 @@
-from django.urls import path
-from .views import markets_view, single_market_view, sellers_view, product_view, single_product_view, single_sellers_view, MarketsView
+from django.urls import path, include
+from .views import markets_view, single_market_view, sellers_view, product_view, single_product_view, single_sellers_view, MarketsView, MarketDetailView, ProductViewSet
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r'product', ProductViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('market/', MarketsView.as_view()),
-    path('market/<int:pk>/', single_market_view, name="market-detail"),
+    path('market/<int:pk>/', MarketDetailView.as_view(), name="market-detail"),
     path('seller/', sellers_view),
     path('seller/<int:pk>/', single_sellers_view),
-    path('product/', product_view),
-    path('product/<int:pk>/', single_product_view),
+    # path('product/', product_view),
+    # path('product/<int:pk>/', single_product_view),
 ]
     
--- market_app/api/views.py
+++ market_app/api/views.py
@@ -1,30 +1,33 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
+
+
 from .serializers import MarketSerializer, SellerSerializer, ProductsSerializer, MarketHyperlinkedSerializer
 from market_app.models import Market, Seller, Product
+
 from rest_framework.views import APIView
 from rest_framework import mixins
 from rest_framework import generics
-
+from rest_framework import viewsets
 from market_app.api import serializers
 
 
+#class based view lowest
+class MarketsView(generics.ListCreateAPIView):
+    queryset = Market.objects.all()
+    serializer_class = MarketHyperlinkedSerializer
 
-class MarketsView(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+
+class MarketDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Market.objects.all()
     serializer_class = MarketSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
 
 
 
+#function based height
 @api_view(['GET', 'POST'])
 def markets_view(request):
     if request.method == "GET":
@@ -41,7 +44,7 @@ def markets_view(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  # Gibt Fehlermeldungen zurück, wenn die Daten ungültig sind
 
 
-
+#function based height
 @api_view(['GET','PUT', 'DELETE'])  # Dekorator, der angibt, dass diese View GET und DELETE Anfragen akzeptiert
 def single_market_view(request, pk):  # pk = primary Key, ist die ID aus der Datenbank
     try:
@@ -64,7 +67,7 @@ def single_market_view(request, pk):  # pk = primary Key, ist die ID aus der Dat
         market.delete()  # Löscht das Market-Objekt aus der Datenbank
         return Response(status=status.HTTP_204_NO_CONTENT)  # Gibt 204 No Content zurück, um erfolgreiche Löschung anzuzeigen
     
-
+#function based height
 @api_view(['GET', 'POST'])
 def sellers_view(request):
     if request.method == "GET":
@@ -81,7 +84,7 @@ def sellers_view(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  # Gibt Fehlermeldungen zurück, wenn die Daten ungültig sind
 
 
-
+#function based height
 @api_view(['GET','PUT', 'DELETE'])  # Dekorator, der angibt, dass diese View GET und DELETE Anfragen akzeptiert
 def single_sellers_view(request, pk):  # pk = primary Key, ist die ID aus der Datenbank
     try:
@@ -94,7 +97,7 @@ def single_sellers_view(request, pk):  # pk = primary Key, ist die ID aus der Da
         return Response(serializer.data)  # Gibt die serialisierten Daten als Antwort zurück
     
     elif request.method == "PUT":  # Prüft, ob die HTTP-Methode PUT ist (für Aktualisierungen)
-        serializer = SellerSerializer(seller, data=request.data, partial=True)  # Erstellt einen Serializer mit dem existierenden Objekt und den neuen Daten, erlaubt partielle Updates
+        serializer = SellerSerializer(seller, data=request.data, partial=False)  # Erstellt einen Serializer mit dem existierenden Objekt und den neuen Daten, erlaubt partielle Updates
         if serializer.is_valid():  # Überprüft, ob die neuen Daten gültig sind
             serializer.save()  # Aktualisiert das existierende Market-Objekt mit den neuen Daten
             return Response(serializer.data, status=status.HTTP_201_CREATED)  # Gibt die aktualisierten Daten und einen 201 Status zurück
@@ -104,7 +107,81 @@ def single_sellers_view(request, pk):  # pk = primary Key, ist die ID aus der Da
         seller.delete()  # Löscht das Market-Objekt aus der Datenbank
         return Response(status=status.HTTP_204_NO_CONTENT)  # Gibt 204 No Content zurück, um erfolgreiche Löschung anzuzeigen
 
+#ModelViewSet
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductsSerializer
+    
 
+
+#viewSet
+class ProductViewSetOld(viewsets.ViewSet):
+    queryset = Product.objects.all()
+    
+    def list(self, request):#all GET
+        serializer = ProductsSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):#single object GET
+        product = get_object_or_404(self.queryset, pk=pk)
+        serializer = ProductsSerializer(product)
+        return Response(serializer.data)
+
+    def create(self, request):#all POST
+        serializer = ProductsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def update(self, request, pk=None): #single PUT
+        product = get_object_or_404(self.queryset, pk=pk)
+        serializer = ProductsSerializer(product, data=request.data, partial=False)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def partial_update(self, request, pk=None): #single PATCH
+        product = get_object_or_404(self.queryset, pk=pk)
+        serializer = ProductsSerializer(product, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def destroy(self, request, pk=None): #single DELETE
+        product = get_object_or_404(self.queryset, pk=pk)
+        serializer = ProductsSerializer(product)
+        product.delete()
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+    
+
+
+
+
+
+
+#class based view height
+class ProductView(APIView):
+    def get(self, request, format=None):
+        snippets = Product.objects.all()
+        serializer = ProductsSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = ProductsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+#function based height
 @api_view(['GET', 'POST'])
 def product_view(request):
     if request.method == "GET":
@@ -118,8 +195,10 @@ def product_view(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    
+
+
+
+ #function based height   
 @api_view(['GET','PUT', 'DELETE'])  # Dekorator, der angibt, dass diese View GET und DELETE Anfragen akzeptiert
 def single_product_view(request, pk):  # pk = primary Key, ist die ID aus der Datenbank
     try:
